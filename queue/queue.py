977c7d836f80b3eab43f47347ad91b117436e973
CAM603 <cameronhawley@comcast.net>
2020-05-11 19:07:18 -0400

    completed queue

--- queue/queue.py
+++ queue/queue.py
@@ -9,17 +9,83 @@ return elements in First In First Out order.
    Make sure the Queue tests pass.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Queue?
+
+Stretch: What if you could only use instances of your Stack class to implement the Queue?
+    What would that look like? How many Stacks would you need? Try it!
 """
+
+
+class Node:
+    def __init__(self, value=None, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
+    def get_value(self):
+        return self.value
+
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, new_next):
+        self.next_node = new_next
+
+
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
-    
+        self.storage = None
+
     def __len__(self):
-        pass
+        return self.size
 
     def enqueue(self, value):
-        pass
+        # adding to the end or tail
+        # wrap value in a new node
+        new_node = Node(value)
+        # increase size
+        self.size += 1
+        # if the storage is None
+        if not self.storage:
+            self.storage = new_node
+        else:
+            # add new node to the last node
+            current = self.storage
+            while current.get_next() is not None:
+                current = current.get_next()
+            current.set_next(new_node)
 
     def dequeue(self):
-        pass
+        # removing from the head/beginning
+        # if nothing stored
+        if not self.storage:
+            return None
+        else:
+            # reduce size
+            self.size -= 1
+            # capture value we want to return
+            # we want value of the current node
+            value = self.storage.get_value()
+            # remove value at the head
+            # update head
+            self.storage = self.storage.get_next()
+            return value
+
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return len(self.storage)
+
+#     def enqueue(self, value):
+#         # adding to the end or tail
+#         self.storage.insert(0, value)
+#         return self.storage
+
+#     def dequeue(self):
+#         # removing from the head/beginning
+#         if self.__len__() is 0:
+#             return None
+#         else:
+#             return self.storage.pop()
--- stack/stack.py
+++ stack/stack.py
@@ -10,16 +10,53 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+
+
+class Node:
+    def __init__(self, value=None, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
+    def get_value(self):
+        return self.value
+
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, new_next):
+        self.next_node = new_next
+
+
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = []
 
     def __len__(self):
-        pass
+        return len(self.storage)
 
     def push(self, value):
-        pass
+        self.storage.append(value)
 
     def pop(self):
-        pass
+        if self.__len__() is 0:
+            return None
+        else:
+            return self.storage.pop()
+
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return len(self.storage)
+
+#     def push(self, value):
+#         self.storage.append(value)
+
+#     def pop(self):
+#         if self.__len__() is 0:
+#             return None
+#         else:
+#             return self.storage.pop()
